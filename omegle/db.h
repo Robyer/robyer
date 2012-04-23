4393bf0d36655fdb830adce59ae922ffd5d00db4
robyer <robyer@1e3d17b3-894c-9a48-22c1-8f862e4e64fc>
2012-04-23 10:02:23 +0000

    Reworked commands (add /spy, /commands, ...) Remember last used question in Question mode. Version bump.

--- omegle/db.h
+++ omegle/db.h
@@ -52,3 +52,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define OMEGLE_KEY_SERVER			"Server"
 #define OMEGLE_KEY_LOGGING			"EnableLogging"
 #define OMEGLE_KEY_SERVER_INFO		"GetServerInfo"
+#define OMEGLE_KEY_LAST_QUESTION	"LastQuestion"
